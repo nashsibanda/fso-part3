9e1bf32fefbea545c9f2c53ce68d355bd71d1b73
Nash Sibanda <nash@Nashs-iMac.local>
2020-10-11 16:51:40 +0100

    uibuild

---
+++ build/precache-manifest.e9c6a44e46166ad48a738c5c3e3e73f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba340ce6c3c2c69393d666a4436fd51c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdef0b6d324aa0cef0e0",
+    "url": "/static/css/main.fe97e6b1.chunk.css"
+  },
+  {
+    "revision": "b7495fad94fc3655be8f",
+    "url": "/static/js/2.6f8a62a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f8a62a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdef0b6d324aa0cef0e0",
+    "url": "/static/js/main.b8dc8a89.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
